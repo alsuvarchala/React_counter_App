45c18ae2cb9000d062a2f17191c95498443b7481
alsuvarchala <lakshmisuvarchalaanumula@gmail.com>
2025-09-28 20:11:51 +0530

    First commit - Colorful Counter App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1> Counter App </h1>
+      <p className="count">Count: {count}</p>
+      <div className="buttons">
+        <button className="btn add" onClick={() => setCount(c => c + 1)}>
+          ➕ Increase
+        </button>
+        <button className="btn sub" onClick={() => setCount(c => c - 1)}>
+          ➖ Decrease
+        </button>
+        <button className="btn reset" onClick={() => setCount(0)}>
+          🔄 Reset
+        </button>
+      </div>
     </div>
   );
 }
